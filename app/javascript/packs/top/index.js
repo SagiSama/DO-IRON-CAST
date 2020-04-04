cbf8ad0ef8e9ee295be0c2a896f18caae35669d1
unisaruul <chinzorigt_saruulgun@unimedia.co.jp>
2020-04-04 17:27:52 +0800

    include js s DO NOT CHANGE

--- app/javascript/packs/top/index.js
+++ app/javascript/packs/top/index.js
@@ -1,52 +1,6 @@
+window.WOW = require('packs/wow.min').WOW
+window.classie = require('packs/classie')
 require('packs/pace.min')
 require('bootstrap')
-
-$(document).ready(function () {
-    $('body').scrollspy({
-        target: '#navbar',
-        offset: 80
-    });
-
-    // Page scrolling feature
-    $('a.page-scroll').bind('click', function(event) {
-        var link = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $(link.attr('href')).offset().top - 50
-        }, 500);
-        event.preventDefault();
-        $("#navbar").collapse('hide');
-    });
-});
-
-var cbpAnimatedHeader = (function() {
-    var docElem = document.documentElement,
-        header = document.querySelector( '.navbar-default' ),
-        didScroll = false,
-        changeHeaderOn = 200;
-    function init() {
-        window.addEventListener( 'scroll', function( event ) {
-            if( !didScroll ) {
-                didScroll = true;
-                setTimeout( scrollPage, 250 );
-            }
-        }, false );
-    }
-    function scrollPage() {
-        var sy = scrollY();
-        if ( sy >= changeHeaderOn ) {
-            $(header).addClass('navbar-scroll')
-        }
-        else {
-            $(header).removeClass('navbar-scroll')
-        }
-        didScroll = false;
-    }
-    function scrollY() {
-        return window.pageYOffset || docElem.scrollTop;
-    }
-    init();
-
-})();
-
-// Activate WOW.js plugin for animation on scrol
-// new WOW().init();
+require('packs/inspinia')
+require('packs/cbpAnimatedHeader')
