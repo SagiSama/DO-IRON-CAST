7b28a6348e56c822b1f6e1dad6f1e4b1df04bad6
unisaruul <chinzorigt_saruulgun@unimedia.co.jp>
2020-04-18 19:24:04 +0800

    active storage image news

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,5 @@
 class News < ApplicationRecord
+  has_many_attached :images
+
   enum status: {inactive: 0, active: 1}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Account.create(name: 'Ganja', email: 'admin@gmail.com', password: '@dm1n_#$%7')
+Account.create([{ name: 'Ganja', email: 'admin@gmail.com', password: '@dm1n_#$%7' },
+                         { name: 'Ganja', email: 'sys_admin@gmail.com', password: '2@3#x%x$i??i' }])
 Product.create([{ name: 'product1', status: Product.statuses[:active]  },
                 { name: 'product2', note: 'note 2', price: 123, status: Product.statuses[:active] },
                 { name: 'inactive product', status: Product.statuses[:inactive] }])
