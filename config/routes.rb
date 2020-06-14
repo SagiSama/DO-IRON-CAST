cc92e2b18a854b436414db472817a5c57a5df566
unisaruul <chinzorigt_saruulgun@unimedia.co.jp>
2020-06-14 15:33:08 +0800

    debug4

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :accounts, controllers: { sessions: 'accounts/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'top#index', as: 'top'
 
   scope :admin do
+    devise_for :accounts, controllers: { sessions: 'accounts/sessions' }
     root to: 'news#index', as: 'dashboard'
     resources :products
     resources :news
